763b750a406736ad9c6a3afb3e9fd1bb3ba789f6
DineshDhanoki <167537800+DineshDhanoki@users.noreply.github.com>
2024-10-09 02:03:28 +0530

    Add files via upload

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -65,16 +65,16 @@ export default function HomePage(props) {
     })
 
 
-  return (
+    return (
         <main className='flex-1  p-4 flex flex-col gap-3 text-center sm:gap-4  justify-center pb-20'>
             <h1 className='font-semibold text-5xl sm:text-6xl md:text-7xl'>Free<span className='text-blue-400 bold'>Scribe</span></h1>
             <h3 className='font-medium md:text-lg'>Record <span className='text-blue-400'>&rarr;</span> Transcribe <span className='text-blue-400'>&rarr;</span> Translate</h3>
             <button onClick={recordingStatus === 'recording' ? stopRecording : startRecording} className='flex specialBtn px-4 py-2 rounded-xl items-center text-base justify-between gap-4 mx-auto w-72 max-w-full my-4'>
                 <p className='text-blue-400'>{recordingStatus === 'inactive' ? 'Record' : `Stop recording`}</p>
                 <div className='flex items-center gap-2'>
-                    {duration !== 0 && (
+                    {/* {duration !== 0 && (
                         <p className='text-sm'>{duration}s</p>
-                    )}
+                    )} */}
                     <i className={"fa-solid duration-200 fa-microphone " + (recordingStatus === 'recording' ? ' text-rose-300' : "")}></i>
                 </div>
             </button>
--- src/utils/whisper.worker.js
+++ src/utils/whisper.worker.js
@@ -1,21 +1,22 @@
-import { pipeline } from "@xenova/transformers";
-import { MessageTypes } from "./presets";
+import { pipeline } from '@xenova/transformers'
+import { MessageTypes } from './presets'
 
-class MyTranscriptionPipleline {
+class MyTranscriptionPipeline {
     static task = 'automatic-speech-recognition'
     static model = 'openai/whisper-tiny.en'
     static instance = null
 
     static async getInstance(progress_callback = null) {
         if (this.instance === null) {
-            this.instance = await pipeline(this.task, null, {progress_callback})
+            this.instance = await pipeline(this.task, null, { progress_callback })
         }
+
         return this.instance
     }
 }
 
 self.addEventListener('message', async (event) => {
-    const {type, audio} = event.data
+    const { type, audio } = event.data
     if (type === MessageTypes.INFERENCE_REQUEST) {
         await transcribe(audio)
     }
@@ -26,18 +27,17 @@ async function transcribe(audio) {
 
     let pipeline
 
-    try{
-        pipeline = await MyTranscriptionPipleline.getInstance
-        (load_model_callback)
+    try {
+        pipeline = await MyTranscriptionPipeline.getInstance(load_model_callback)
     } catch (err) {
-        console.log(error.message)
+        console.log(err.message)
     }
 
     sendLoadingMessage('success')
 
     const stride_length_s = 5
 
-    const generationTracker =  new GenerationTracker(pipeline, stride_length_s)
+    const generationTracker = new GenerationTracker(pipeline, stride_length_s)
     await pipeline(audio, {
         top_k: 0,
         do_sample: false,
@@ -51,11 +51,12 @@ async function transcribe(audio) {
 }
 
 async function load_model_callback(data) {
-    const {status} = data
+    const { status } = data
     if (status === 'progress') {
-    const {file, progress, loaded, total} = data
-    sendDownloadingMessage(file, progress, loaded, total)
-}}
+        const { file, progress, loaded, total } = data
+        sendDownloadingMessage(file, progress, loaded, total)
+    }
+}
 
 function sendLoadingMessage(status) {
     self.postMessage({
@@ -85,12 +86,12 @@ class GenerationTracker {
     }
 
     sendFinalResult() {
-        self.postMessage({type: MessageTypes.INFERENCE_DONE})
+        self.postMessage({ type: MessageTypes.INFERENCE_DONE })
     }
 
     callbackFunction(beams) {
         this.callbackFunctionCounter += 1
-        if(this.callbackFunctionCounter % 10 !== 0) {
+        if (this.callbackFunctionCounter % 10 !== 0) {
             return
         }
 
@@ -102,7 +103,7 @@ class GenerationTracker {
         const result = {
             text,
             start: this.getLastChunkTimestamp(),
-            end:undefined
+            end: undefined
         }
 
         createPartialResultMessage(result)
@@ -110,11 +111,11 @@ class GenerationTracker {
 
     chunkCallback(data) {
         this.chunks.push(data)
-        const [text, {chunks}] = this.pipeline.tokenizer._decode_asr(
+        const [text, { chunks }] = this.pipeline.tokenizer._decode_asr(
             this.chunks,
             {
                 time_precision: this.time_precision,
-                return_precision: true,
+                return_timestamps: true,
                 force_full_sequence: false
             }
         )
@@ -123,31 +124,35 @@ class GenerationTracker {
             return this.processChunk(chunk, index)
         })
 
+
         createResultMessage(
             this.processed_chunks, false, this.getLastChunkTimestamp()
-        )}
+        )
+    }
 
-        getLastChunkTimestamp() {
-            if (this.processed_chunks.length === 0) {
-                return 0
-            }
+    getLastChunkTimestamp() {
+        if (this.processed_chunks.length === 0) {
+            return 0
+        }
+    }
+
+    processChunk(chunk, index) {
+        const { text, timestamp } = chunk
+        const [start, end] = timestamp
+
+        return {
+            index,
+            text: `${text.trim()}`,
+            start: Math.round(start),
+            end: Math.round(end) || Math.round(start + 0.9 * this.stride_length_s)
         }
 
-        processChunk(chunk, index) {
-            const{text, timestamp} = chunk
-            const [start, end] = timestamp
-            return {
-                index,
-                text: `${text.trim()}`,
-                start: Math.round(start),
-                end: Math.round(end) || Math.round(start + 0.9 * this.stride_length_s)
-            }
     }
 }
 
 function createResultMessage(results, isDone, completedUntilTimestamp) {
     self.postMessage({
-        type: MessageTypes.Result,
+        type: MessageTypes.RESULT,
         results,
         isDone,
         completedUntilTimestamp
